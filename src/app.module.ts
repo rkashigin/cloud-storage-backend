bd3066dcb5cdd34bd6a015414a0773eee3f7001e
Roman Kashigin <r.kashigin@yandex.ru>
2023-05-15 22:12:56 +0300

    Добавил конфиги для пользователей и файлов + прикрутил базовую загрузку

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
+import { FileEntity } from './files/entities/file.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5444,
+      database: 'cloudstorage',
+      entities: [UserEntity, FileEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
